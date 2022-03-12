b46bf799b5295a0af2bd111bed793f3b7ce6c54c
dobricsandra <dobric.sandra@hotmail.com>
2022-03-13 00:56:33 +0100

    Enable searching from the search bar

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -2,16 +2,31 @@ import { BiAddToQueue } from "react-icons/bi";
 import { NavigationStyled } from "./Navigation.style";
 import { useNavigate } from "react-router-dom";
 import { webshopRoutes } from "../../routes/routesList";
+import { useEffect, useState } from "react";
 
 const Navigation = () => {
     const navigate = useNavigate();
+
+    const [search, setSearch] = useState("");
+
+    const handleInput = (event: any) => {
+        setSearch(event.target.value);
+    };
+
+    useEffect(() => {
+        navigate(`/coupons?webshopName=${search}`);
+    }, [search]);
+
     return (
         <>
             <NavigationStyled>
                 <div className="upper-header">
                     <div>
                         <label>Aktualni kuponi na jednom mjestu.</label>
-                        <input placeholder="Search coupons..." />
+                        <input
+                            placeholder="Search coupons..."
+                            onInput={handleInput}
+                        />
                     </div>
                     <button onClick={() => navigate("/coupons/add")}>
                         <BiAddToQueue size={24} />
@@ -25,8 +40,20 @@ const Navigation = () => {
                     </ul>
                     <div className="border"></div>
                     <ul>
-                        <li>Most popular</li>
-                        <li>Newest</li>
+                        <li
+                            onClick={() =>
+                                navigate("/coupons?sortBy=Popularity")
+                            }
+                        >
+                            Most popular
+                        </li>
+                        <li
+                            onClick={() =>
+                                navigate("/coupons?sortBy=DateAdded")
+                            }
+                        >
+                            Newest
+                        </li>
                     </ul>
                     <div className="border"></div>
                     <ul>
